366882e56a04d1bfc14c7e1a5d79ba3752ab3b72
Ashwini Kumari <ashwini.swe@gmail.com>
2024-07-16 12:27:17 +0530

    crated project vite + react + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1 className='text-red-500 underline'>
+      Hello world!
+    </h1>
+
+      
+    </>
+  )
+}
+
+export default App
